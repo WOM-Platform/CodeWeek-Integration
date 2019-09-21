927e692f2b41e8dbff543f14a982337744630d23
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-09-21 15:40:36 +0200

    Simple certificate validation

--- src/Web/Web/Controllers/HomeController.cs
+++ src/Web/Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -36,6 +38,40 @@ namespace WomPlatform.Web.Api.Controllers {
             Logger.LogDebug("Index view");
 
             return View("Index");
+        }
+
+        private static readonly Regex _urlMatcher = new Regex(
+            @"http(s)?://codeweek-s3.s3.amazonaws.com/certificates/(?'id'\d*)-[^\.]*.pdf",
+            RegexOptions.Compiled | RegexOptions.Singleline
+        );
+
+        private static readonly HttpClient _client = new HttpClient();
+
+        [HttpPost("/convert")]
+        public async Task<IActionResult> Process(string certificateUrl) {
+            Logger.LogInformation("Processing certificate {0}", certificateUrl);
+
+            var match = _urlMatcher.Match(certificateUrl);
+            if(!match.Success) {
+                return StatusCode(400);
+            }
+
+            int certificateId = int.Parse(match.Groups["id"].Value);
+            if(Database.Certificates.Where(c => c.CertificateId == certificateId).Count() > 0) {
+                Logger.LogError("Certificate {0} already converted", certificateId);
+                return StatusCode(400);
+            }
+
+            var certificateCheckResponse = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Head, certificateUrl));
+            if(!certificateCheckResponse.IsSuccessStatusCode) {
+                Logger.LogError("Certificate does not exist on server, status {0}", certificateCheckResponse.StatusCode);
+                return StatusCode(400);
+            }
+            Logger.LogDebug("Certificate validated, HEAD request returns status OK");
+
+            return Content("Success!");
+
+            //return RedirectToAction(nameof(Index));
         }
 
     }
