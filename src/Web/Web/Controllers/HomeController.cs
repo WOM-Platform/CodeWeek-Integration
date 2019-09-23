cd27bac1580e551a778d2a57a1af90589a597389
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-09-23 19:00:04 +0200

    Working data extraction from event page and error checking

--- src/Web/Web/Controllers/HomeController.cs
+++ src/Web/Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using WomPlatform.Web.Api.ViewModel;
 
 namespace WomPlatform.Web.Api.Controllers {
@@ -42,53 +44,137 @@ namespace WomPlatform.Web.Api.Controllers {
         public IActionResult Index() {
             Logger.LogDebug("Index view");
 
-            return View("Index");
+            var viewModel = GetRedirectData<ConversionModelViewModel>() ?? new ConversionModelViewModel();
+
+            return View("Index", viewModel);
         }
 
-        private static readonly Regex _urlMatcher = new Regex(
-            @"http(s)?://codeweek-s3.s3.amazonaws.com/certificates/(?'id'\d*)-[^\.]*.pdf",
-            RegexOptions.Compiled | RegexOptions.Singleline
+        private static readonly Regex _certificateUrlMatcher = new Regex(
+            @"^http(s)?://codeweek-s3.s3.amazonaws.com/certificates/(?'id'\d*)-[^\.]*.pdf",
+            RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.ExplicitCapture
+        );
+
+        private static readonly Regex _eventPageUrlMatcher = new Regex(
+            @"^http(s)?://codeweek.eu/view/(?'id'\d*)/",
+            RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.ExplicitCapture
+        );
+
+        private static readonly Regex _eventPageExtractor = new Regex(
+            @"geoposition""\s*:\s*""(?<lat>[\d.]*)\s*,\s*(?<long>[\d.]*)""",
+            RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.ExplicitCapture
         );
 
         private static readonly HttpClient _client = new HttpClient();
 
+        static HomeController() {
+            _client.DefaultRequestHeaders.Referrer = new Uri("https://wom.social");
+            _client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("WomConverter", "1.0"));
+            _client.DefaultRequestHeaders.AcceptLanguage.Add(new System.Net.Http.Headers.StringWithQualityHeaderValue("en-US"));
+        }
+
         [HttpPost("/convert")]
-        public async Task<IActionResult> Process(string certificateUrl) {
+        public async Task<IActionResult> Process(string certificateUrl, string eventPageUrl) {
             Logger.LogInformation("Processing certificate {0}", certificateUrl);
 
-            var match = _urlMatcher.Match(certificateUrl);
-            if(!match.Success) {
-                return StatusCode(400);
+            var redirectModel = new ConversionModelViewModel {
+                CertificateUrl = certificateUrl,
+                EventPageUrl = eventPageUrl
+            };
+
+            var certificateMatch = _certificateUrlMatcher.Match(certificateUrl ?? string.Empty);
+            if(!certificateMatch.Success) {
+                redirectModel.Error = ConversionModelViewModel.ConversionError.CertificateUrlInvalid;
+                return RedirectToActionWithData(nameof(Index), redirectModel);
+            }
+            var eventPageMatch = _eventPageUrlMatcher.Match(eventPageUrl ?? string.Empty);
+            if(!eventPageMatch.Success) {
+                redirectModel.Error = ConversionModelViewModel.ConversionError.EventUrlInvalid;
+                return RedirectToActionWithData(nameof(Index), redirectModel);
             }
 
-            int certificateId = int.Parse(match.Groups["id"].Value);
+            int certificateId = int.Parse(certificateMatch.Groups["id"].Value, CultureInfo.InvariantCulture);
+            int eventId = int.Parse(eventPageMatch.Groups["id"].Value, CultureInfo.InvariantCulture);
+            if(certificateId != eventId) {
+                redirectModel.Error = ConversionModelViewModel.ConversionError.EventCertificateIdDifform;
+                return RedirectToActionWithData(nameof(Index), redirectModel);
+            }
+
             if(Database.Certificates.Where(c => c.CertificateId == certificateId).Count() > 0) {
                 Logger.LogError("Certificate {0} already converted", certificateId);
-                return StatusCode(400);
+
+                redirectModel.Error = ConversionModelViewModel.ConversionError.CertificateAlreadyConverted;
+                return RedirectToActionWithData(nameof(Index), redirectModel);
             }
 
             var certificateCheckResponse = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Head, certificateUrl));
             if(!certificateCheckResponse.IsSuccessStatusCode) {
                 Logger.LogError("Certificate does not exist on server, status {0}", certificateCheckResponse.StatusCode);
-                return StatusCode(400);
+
+                redirectModel.Error = ConversionModelViewModel.ConversionError.CertificateNotExisting;
+                return RedirectToActionWithData(nameof(Index), redirectModel);
             }
             Logger.LogDebug("Certificate validated, HEAD request returns status OK");
 
-            var instrument = WomClient.CreateInstrument(1, KeyManager.InstrumentPrivateKey);
-            var womResult = await instrument.RequestVouchers(new Connector.Models.VoucherCreatePayload.VoucherInfo[] {
-                new Connector.Models.VoucherCreatePayload.VoucherInfo {
-                    Aim = "C",
-                    Count = 1,
-                    Latitude = 42.934872,
-                    Longitude = 12.609390,
-                    Timestamp = DateTime.UtcNow
-                }
-            });
+            var eventUrlCheckResponse = await _client.GetAsync(eventPageUrl);
+            if(!eventUrlCheckResponse.IsSuccessStatusCode) {
+                Logger.LogError("Failed to check event page URL, status {0}", eventUrlCheckResponse.StatusCode);
 
-            return View("Vouchers", new ConversionResult {
-                OtcCode = womResult.Otc.ToString("N"),
-                Password = womResult.Password
-            });
+                redirectModel.Error = ConversionModelViewModel.ConversionError.EventUrlFailedToCheck;
+                return RedirectToActionWithData(nameof(Index), redirectModel);
+            }
+            var eventUrlCheckContents = await eventUrlCheckResponse.Content.ReadAsStringAsync();
+            var eventUrlMatch = _eventPageExtractor.Match(eventUrlCheckContents);
+            if(!eventUrlMatch.Success) {
+                Logger.LogError("Failed to find geoposition values in event page");
+
+                redirectModel.Error = ConversionModelViewModel.ConversionError.EventUrlFailedToCheck;
+                return RedirectToActionWithData(nameof(Index), redirectModel);
+            }
+            double eventLat = double.Parse(eventUrlMatch.Groups["lat"].Value, CultureInfo.InvariantCulture);
+            double eventLong = double.Parse(eventUrlMatch.Groups["long"].Value, CultureInfo.InvariantCulture);
+
+            Logger.LogInformation("Generating vouchers for event {0} at location {1},{2}", eventId, eventLat, eventLong);
+
+            try {
+                var instrument = WomClient.CreateInstrument(1, KeyManager.InstrumentPrivateKey);
+                var (otc, password) = await instrument.RequestVouchers(new Connector.Models.VoucherCreatePayload.VoucherInfo[] {
+                    new Connector.Models.VoucherCreatePayload.VoucherInfo {
+                        Aim = "C",
+                        Count = 1,
+                        Latitude = eventLat,
+                        Longitude = eventLong,
+                        Timestamp = DateTime.UtcNow
+                    }
+                });
+
+                return View("Vouchers", new ConversionResult {
+                    OtcCode = otc.ToString("N"),
+                    Password = password
+                });
+            }
+            catch(Exception ex) {
+                Logger.LogError(0, ex, "Failed to generate vouchers");
+
+                redirectModel.Error = ConversionModelViewModel.ConversionError.InternalWomGenerationError;
+                return RedirectToActionWithData(nameof(Index), redirectModel);
+            }
+        }
+
+        private const string RedirectTempViewModelKey = "redirectData";
+
+        private IActionResult RedirectToActionWithData(string actionName, object data) {
+            if(data != null) {
+                TempData[RedirectTempViewModelKey] = JsonConvert.SerializeObject(data);
+            }
+            return RedirectToAction(actionName);
+        }
+
+        private T GetRedirectData<T>() where T : class {
+            var data = TempData[RedirectTempViewModelKey]?.ToString();
+            if(data == null) {
+                return null;
+            }
+            return JsonConvert.DeserializeObject(data, typeof(T)) as T;
         }
 
     }
---
+++ src/Web/Web/ViewModel/ConversionModelViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WomPlatform.Web.Api.ViewModel {
+
+    public class ConversionModelViewModel {
+
+        public enum ConversionError {
+            EventUrlInvalid,
+            CertificateUrlInvalid,
+            CertificateAlreadyConverted,
+            CertificateNotExisting,
+            EventUrlFailedToCheck,
+            InternalWomGenerationError,
+            EventCertificateIdDifform
+        }
+
+        public ConversionError? Error { get; set; } = null;
+
+        public string CertificateUrl { get; set; }
+
+        public string EventPageUrl { get; set; }
+
+    }
+
+}
