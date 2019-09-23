84cc43413dc538556c266c78f68346d0101a2bdf
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-09-23 19:26:51 +0200

    Add QR Code rendering

---
+++ src/Web/Web/Controllers/RenderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WomPlatform.Web.Api.Controllers {
+
+    [Route("render")]
+    public class RenderController : Controller {
+
+        public IActionResult Index(string url) {
+            if(url == null || !url.StartsWith("https://wom.social/")) {
+                return StatusCode(400);
+            }
+
+            var qrCodeData = QRCoder.PngByteQRCodeHelper.GetQRCode(url, QRCoder.QRCodeGenerator.ECCLevel.M, 15);
+            return File(qrCodeData, "image/png");
+        }
+
+    }
+
+}
