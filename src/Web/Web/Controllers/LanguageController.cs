fa28ac83981f86d763fe650864dd42cfd5d592f0
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-09-28 08:11:41 +0200

    Add language switching

---
+++ src/Web/Web/Controllers/LanguageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WomPlatform.Web.Api.Controllers {
+
+    [Route("language")]
+    public class LanguageController : Controller {
+
+        [HttpGet("set")]
+        public IActionResult Set(string language) {
+            var v = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(language));
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, v);
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+    }
+
+}
